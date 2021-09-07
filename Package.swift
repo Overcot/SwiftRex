0117acc31a5c9a6b12fb2d5c3d79704f6ab0f834
Alexey Ivashko <mcivashko@gmail.com>
2021-09-07 17:21:32 +0300

    Remove unused files and targerts
    Remove RxSwift && ReactiveSwift

--- Package.swift
+++ Package.swift
@@ -3,30 +3,19 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftRex",
-    platforms: [.macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v3)],
+    platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
     products: [
         .library(name: "CombineRex", targets: ["SwiftRex", "CombineRex"]),
-        .library(name: "ReactiveSwiftRex", targets: ["SwiftRex", "ReactiveSwiftRex"]),
-        .library(name: "RxSwiftRex", targets: ["SwiftRex", "RxSwiftRex"]),
-
         .library(name: "CombineRexDynamic", type: .dynamic, targets: ["SwiftRex", "CombineRex"]),
-        .library(name: "ReactiveSwiftRexDynamic", type: .dynamic, targets: ["SwiftRex", "ReactiveSwiftRex"]),
-        .library(name: "RxSwiftRexDynamic", type: .dynamic, targets: ["SwiftRex", "RxSwiftRex"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0"),
-        .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "6.5.0")
     ],
     targets: [
         .target(name: "SwiftRex", exclude: ["CodeGeneration/Templates"]),
         .target(name: "CombineRex", dependencies: ["SwiftRex"]),
-        .target(name: "ReactiveSwiftRex", dependencies: ["SwiftRex", "ReactiveSwift"]),
-        .target(name: "RxSwiftRex", dependencies: ["SwiftRex", "RxSwift"]),
 
         .testTarget(name: "SwiftRexTests", dependencies: ["SwiftRex"]),
         .testTarget(name: "CombineRexTests", dependencies: ["CombineRex"]),
-        .testTarget(name: "ReactiveSwiftRexTests", dependencies: ["ReactiveSwiftRex"]),
-        .testTarget(name: "RxSwiftRexTests", dependencies: ["RxSwiftRex"])
     ],
     swiftLanguageVersions: [.v5]
 )
